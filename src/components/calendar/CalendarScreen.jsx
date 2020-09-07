c32b3cd5e788336be4bad9cd2c584643e24c3445
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-09-07 18:45:36 -0300

    base work calendar

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -3,7 +3,9 @@ import Navbar from '../ui/Navbar'
 import {Calendar,momentLocalizer} from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-
+import { messages } from '../../helpers/calendar-messages'
+import 'moment/locale/es'
+moment.locale('es');
 const localizer = momentLocalizer(moment); // or globalizeLocalizer
 
 const events=[{
@@ -14,6 +16,20 @@ const events=[{
 }];
 
 const CalendarScreen = () => {
+    const eventStyleGetter=(event,start,end,isSelected)=>{
+        const style={
+            backgroundColor:'#367CF7',
+            borderRadius:'0p',
+            opacity:0.8,
+            display:'block',
+            color:'#FFF'
+        }
+        
+        return{
+            style
+        }
+    };
+
     return ( 
         <div className="calendar-screen">
             <Navbar/>
@@ -23,6 +39,8 @@ const CalendarScreen = () => {
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
+                messages={messages}
+                eventPropGetter={eventStyleGetter}
             />
         </div> 
     );
